b380015e555a208b23925c63c56e8277ddf0ad9f
dyrnq <dyrnq@qq.com>
2025-06-25 09:52:43 +0800

    Add flink IcebergRestCatalog

---
+++ README.md
@@ -0,0 +1,27 @@
+## flink-coding
+
+### build
+
+```bash
+./mvnw clean package -Dmaven.test.skip=true
+```
+
+### flink run
+
+### IcebergRestCatalog
+```bash
+## optional curl jar file from remote 
+curl -o flink-coding-1.0.0.jar -fsSL# http://192.168.1.171:13000/target/flink-coding-1.0.0.jar
+## run
+flink \
+run \
+--target remote \
+--class sample.IcebergRestCatalog \
+flink-coding-1.0.0.jar \
+--user.timezone Asia/Shanghai \
+--rest.uri http://192.168.1.152:9001/iceberg \
+--s3.endpoint http://192.168.1.131:19000 \
+--aws.region us-east-1 \
+--aws.accessKeyId xxxxxx \
+--aws.secretAccessKey xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+```
---
+++ src/main/scala/sample/IcebergRestCatalog.scala
@@ -0,0 +1,73 @@
+package sample
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+
+object IcebergRestCatalog {
+
+  def main(args: Array[String]): Unit = {
+
+    val params = ParameterTool.fromArgs(args)
+    val awsRegion = params.get("aws.region", "us-east-1")
+    val restUri = params.get("rest.uri", "")
+    val s3Endpoint = params.get("s3.endpoint", "")
+    val awsAccessKeyId = params.get("aws.accessKeyId", "")
+    val awsSecretAccessKey = params.get("aws.secretAccessKey", "")
+
+//    val awsRegion = System.getProperty("aws.region", "us-east-1")
+//    val awsAccessKeyId = System.getProperty("aws.accessKeyId", "default_value")
+//    val awsSecretAccessKey = System.getProperty("aws.secretAccessKey", "default_value")
+//    val s3Endpoint = System.getProperty("s3.endpoint", "default_value")
+//    val restUri = System.getProperty("rest.uri", "default_value")
+
+
+    val settings = EnvironmentSettings.newInstance().inStreamingMode()
+      .withConfiguration(setConf())
+      .build() //读设置
+    val tableEnv = TableEnvironment.create(settings)
+    val config = tableEnv.getConfig;
+
+    config.set("key", "value")
+    config.set("anotherKey", "anotherValue")
+
+    val createCatalogSql=
+      s"""
+         |CREATE CATALOG rest_catalog WITH (
+         |  'type'='iceberg',
+         |  'catalog-type'='rest',
+         |  'uri'='$restUri',
+         |  's3.endpoint'='$s3Endpoint',
+         |  'client.region'='$awsRegion',
+         |  'client.credentials-provider'='sample.CustomCredentialProvider',
+         |  'client.credentials-provider.accessKeyId'='$awsAccessKeyId',
+         |  'client.credentials-provider.secretAccessKey'='$awsSecretAccessKey'
+         |);
+         |""".stripMargin;
+    print(createCatalogSql)
+    tableEnv.executeSql(createCatalogSql)
+    tableEnv.executeSql(
+      """
+        |SHOW catalogs;
+            """.stripMargin).print()
+
+    tableEnv.executeSql(
+      """
+        |SHOW catalogs;
+            """.stripMargin).print()
+
+    tableEnv.executeSql(
+      """
+        |select * from rest_catalog.testdb.my_table;
+            """.stripMargin).print()
+
+
+  }
+
+  private def setConf(): Configuration = {
+    val config = new Configuration()
+    config.setString("execution.checkpointing.interval", "10000")
+    config.setString("sql-client.execution.result-mode", "tableau")
+    config
+  }
+}
