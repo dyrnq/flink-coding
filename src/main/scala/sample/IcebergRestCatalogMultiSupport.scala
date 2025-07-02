a0386890a701ab1fbcbc4bc90ee845f7616c301a
dyrnq <dyrnq@qq.com>
2025-07-02 13:18:52 +0800

    Create IcebergRestCatalogMultiSupport.scala

---
+++ src/main/scala/sample/IcebergRestCatalogMultiSupport.scala
@@ -0,0 +1,97 @@
+package sample
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
+
+object IcebergRestCatalogMultiSupport {
+
+
+
+  //https://gravitino.apache.org/docs/0.9.0-incubating/iceberg-rest-service/#multi-catalog-support
+
+
+  def main(args: Array[String]): Unit = {
+
+    val params = ParameterTool.fromArgs(args)
+
+    val sql = params.get("sql", "select 1=1;")
+
+
+    val settings = EnvironmentSettings.newInstance().inStreamingMode()
+      .withConfiguration(setConf())
+      .build() //读设置
+    val tableEnv = TableEnvironment.create(settings)
+    val config = tableEnv.getConfig;
+
+    config.set("key", "value")
+    config.set("anotherKey", "anotherValue")
+
+
+    tableEnv.executeSql(
+      s"""
+         |CREATE CATALOG foo_rest_catalog WITH (
+         |  'type'='iceberg',
+         |  'catalog-type'='rest',
+         |  'uri'='http://192.168.6.159:29001/iceberg',
+         |  'prefix' = 'foo',
+         |  's3.endpoint'='http://192.168.6.159:9000',
+         |  'client.region'='us-east-1',
+         |  'client.credentials-provider'='sample.CustomCredentialProvider',
+         |  'client.credentials-provider.accessKeyId'='vUR3oLMF5ds8gWCP',
+         |  'client.credentials-provider.secretAccessKey'='odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD'
+         |);
+         |""".stripMargin
+    )
+
+    tableEnv.executeSql(
+      s"""
+         |CREATE CATALOG bar_rest_catalog WITH (
+         |  'type'='iceberg',
+         |  'catalog-type'='rest',
+         |  'uri'='http://192.168.6.159:29001/iceberg',
+         |  'prefix' = 'bar',
+         |  's3.endpoint'='http://192.168.6.159:9200',
+         |  'client.region'='us-east-1',
+         |  'client.credentials-provider'='sample.CustomCredentialProvider',
+         |  'client.credentials-provider.accessKeyId'='vUR3oLMF5ds8gWCP',
+         |  'client.credentials-provider.secretAccessKey'='odWFIZukYrw9dY0G5ezDKMZWbhU0S4oD'
+         |);
+         |""".stripMargin
+    )
+
+
+    tableEnv.executeSql(
+      """
+        |SHOW catalogs;
+            """.stripMargin).print()
+
+
+    tableEnv.executeSql(
+      """
+        |SHOW DATABASES from foo_rest_catalog;
+            """.stripMargin).print()
+
+    tableEnv.executeSql(
+      """
+        |SHOW DATABASES from bar_rest_catalog;
+            """.stripMargin).print()
+
+
+    tableEnv.executeSql(
+      s"""
+         |$sql
+            """.stripMargin).print()
+
+
+  }
+
+  private def setConf(): Configuration = {
+    val config = new Configuration()
+    config.setString("execution.checkpointing.interval", "10000")
+    config.setString("sql-client.execution.result-mode", "tableau")
+    config
+  }
+
+
+}
